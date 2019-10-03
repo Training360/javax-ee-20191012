31d7af1fda482f6cb1d4e131edc99b97f6ae8a4e
vicziani <viczian.istvan@gmail.com>
2019-10-03 14:29:30 +0200

    sse broadcast

--- empapp/src/main/java/empapp/WarningResource.java
+++ empapp/src/main/java/empapp/WarningResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.sse.OutboundSseEvent;
 import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseBroadcaster;
 import javax.ws.rs.sse.SseEventSink;
 
 @Path("warning")
@@ -14,11 +15,16 @@ public class WarningResource {
     @Inject
     private WarningService warningService;
 
+    private static Sse sse;
+
+    private static SseBroadcaster broadcaster;
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Status crateWarning(WarningCommand command) {
         warningService.sendWarning(command);
+        broadcaster.broadcast(sse.newEvent(command.getMessage()));
         return new Status("ok");
     }
 
@@ -26,16 +32,22 @@ public class WarningResource {
     @Path("stream")
     @Produces(MediaType.SERVER_SENT_EVENTS)
     public void listWarnings(@Context SseEventSink sseEventSink,
-                             @Context Sse see) {
-        for (int i = 0; i < 10;i++) {
-            OutboundSseEvent event = see.newEvent("hello from sse");
-            sseEventSink.send(event);
-            try {
-                Thread.sleep(2000);
-            }
-            catch (InterruptedException ie) {
-                throw new IllegalStateException("Interrupted", ie);
-            }
+                             @Context Sse sse) {
+//        for (int i = 0; i < 10;i++) {
+//            OutboundSseEvent event = see.newEvent("hello from sse");
+//            sseEventSink.send(event);
+//            try {
+//                Thread.sleep(2000);
+//            }
+//            catch (InterruptedException ie) {
+//                throw new IllegalStateException("Interrupted", ie);
+//            }
+//        }
+        if (broadcaster == null) {
+            this.sse =  sse;
+            broadcaster = sse.newBroadcaster();
         }
+        broadcaster.register(sseEventSink);
     }
+
 }
