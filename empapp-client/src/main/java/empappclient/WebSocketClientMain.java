d577b46160d2e8083a572a9e880c061835af4057
vicziani <viczian.istvan@gmail.com>
2019-10-03 13:16:08 +0200

    Latch

--- empapp-client/src/main/java/empappclient/WebSocketClientMain.java
+++ empapp-client/src/main/java/empappclient/WebSocketClientMain.java
@@ -3,17 +3,31 @@ package empappclient;
 import javax.websocket.*;
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+
 @ClientEndpoint
 public class WebSocketClientMain {
 
+    private CountDownLatch countDownLatch;
+
+    public WebSocketClientMain(CountDownLatch countDownLatch) {
+        this.countDownLatch = countDownLatch;
+    }
+
     public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         try {
             container.connectToServer(
-                    new WebSocketClientMain(),
+                    new WebSocketClientMain(countDownLatch),
                     URI.create("ws://localhost:8080/empapp/socket/warning"));
         } catch (DeploymentException | IOException e) {
             throw new IllegalStateException("Can not create listener", e);
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException ie) {
+            throw new IllegalStateException("Countdownlatch has interrupted", ie);
         }
     }
 
