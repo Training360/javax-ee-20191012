b92e84eb1d27f0c866b5007b6c328e3814346060
vicziani <viczian.istvan@gmail.com>
2019-10-03 13:09:32 +0200

    Server

--- empapp/src/main/java/empapp/WarningMessageDriven.java
+++ empapp/src/main/java/empapp/WarningMessageDriven.java
@@ -2,6 +2,7 @@ package empapp;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
+import javax.inject.Inject;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -15,11 +16,15 @@ import javax.jms.TextMessage;
 })
 public class WarningMessageDriven implements MessageListener {
 
+    @Inject
+    private WarningWebsocketServer warningWebsocketServer;
+
     @Override
     public void onMessage(Message message) {
             try {
                 String messageText = message.getBody(String.class);
                 System.out.println(messageText);
+                warningWebsocketServer.broadcast(messageText);
             }
             catch (JMSException e) {
                 throw new IllegalStateException("Can not read message", e);
--- empapp/src/main/java/empapp/WarningWebsocketServer.java
+++ empapp/src/main/java/empapp/WarningWebsocketServer.java
@@ -1,25 +1,48 @@
 package empapp;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @ApplicationScoped
 @ServerEndpoint("/socket/warning")
 public class WarningWebsocketServer {
 
+    private List<Session> sessions = Collections.synchronizedList(new ArrayList<>());
+
+    public void broadcast(String message) {
+        for (Session session: sessions) {
+            try {
+                session.getBasicRemote().sendText(message);
+            }
+            catch (IOException ioe) {
+                throw new IllegalStateException("Error reply on websocket", ioe);
+            }
+        }
+    }
+
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("Client connected");
+        sessions.add(session);
         try {
             session.getBasicRemote().sendText("Welcome");
         }
         catch (IOException ioe) {
             throw new IllegalStateException("Error reply on websocket", ioe);
         }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session);
     }
 
     @OnMessage
